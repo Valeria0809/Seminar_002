4862d7c13b4824c0eb4583bfbac6ea1e8934cee0
VALERIA <valeri.tereshenkova@icloud.com>
2023-04-24 23:07:02 +0300

    Task 11

---
+++ task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую
+// цифру этого числа 
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// int number = new Random().Next(100,1000);
+// System.Console.WriteLine(number);
+// int ic = number / 100;
+// int j = number % 10;
+// int result = ic * 10 + j;
+// System.Console.WriteLine(result);
+
+int number = new Random().Next(100,1000);
+System.Console.WriteLine(number);
+System.Console.WriteLine($"{number/100}{number%10}");
